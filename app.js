428044300eb413385794cf35508b2e5b8be3873f
EvgeniiaRiaguzova <101260554+EvgeniiaRiaguzova@users.noreply.github.com>
2022-03-30 11:14:04 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+let count = 1
+function renderImage() {
+let element = document.getElementById('pokemon')
+element.innerHTML = `<img class="poke" src="https://tinyurl.com/ironhack-pokemons/${count}.svg"/>`
+}
+
+let minusElement = document.getElementById('prev')
+minusElement.onclick = function(){
+    if(count > 1) { 
+        count = count -1
+        renderImage()
+    }
+}
+
+let plusElement = document.getElementById('next')
+
+plusElement.onclick = function(){
+    if (count < 650){
+        count = count + 1
+    renderImage()
+    }
+    
+}
+
+renderImage ()
